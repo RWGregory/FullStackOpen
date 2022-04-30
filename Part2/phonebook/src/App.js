f85875dacfbf198c5a9ab39deaea811d96c6cdf0
RWGregory <91198091+RWGregory@users.noreply.github.com>
2022-04-30 12:40:07 -0400

    Exercise 2.19-2.20

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import ContactForm from './ContactForm'
 import Directory from './Directory'
 import Filter from './Filter'
+import Notification from './Notification'
 import contactService from './services/contacts.js'
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState([])
   const [newQuery, setNewQuery] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     contactService.getContacts().then((response) => setContacts(response.data))
@@ -25,24 +28,33 @@ const App = () => {
     e.preventDefault()
     if (names.includes(newName)) {
       const indexOfName = names.indexOf(newName)
-      const filteredContacts = (phonebook) =>
-        phonebook.filter((contact, index) => phonebook[index] !== newContact)
       window.confirm(
         `${newName} is already added to phonebook. Click OK to replace the old number.`,
       )
       contactService
         .editContact(contacts[indexOfName].id, newContact)
         .then((response) => {
-          contactService
-            .getContacts()
-            .then((response) => setContacts(response.data))
-          console.log(response)
+          setNotification(
+            `${response.data.name}'s phone number has been updated!`,
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+          contactService.getContacts().then((response) => {
+            setContacts(response.data)
+          })
         })
+
       return
     }
-    contactService
-      .addContact(newContact)
-      .then((response) => setContacts(contacts.concat(response.data)))
+    contactService.addContact(newContact).then((response) => {
+      setContacts(contacts.concat(response.data))
+
+      setNotification(`${response.data.name} has been added to your phonebook!`)
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
+    })
     setNewName('')
     setNewNumber([])
   }
@@ -62,12 +74,33 @@ const App = () => {
 
   const handleRemove = (e) => {
     if (window.confirm(`Delete ${e.target.name}?`)) {
-      contactService.removeContact(e.target.id).then((response) => {
-        contactService
-          .getContacts()
-          .then((response) => setContacts(response.data))
-        console.log(response)
-      })
+      contactService
+        .removeContact(e.target.id)
+        .then((response) => {
+          setNotification(
+            `${e.target.name} has been removed from your phonebook!`,
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+          contactService
+            .getContacts()
+            .then((response) => setContacts(response.data))
+        })
+        .catch((error) => {
+          console.log(error)
+
+          setNotification(
+            `${e.target.name} has not been found. They may have been removed. The current phonebook will be refreshed.`,
+          )
+          setError((prev) => !prev)
+          setTimeout(() => {
+            setNotification(null)
+            contactService
+              .getContacts()
+              .then((response) => setContacts(response.data))
+          }, 5000)
+        })
     }
   }
 
@@ -76,9 +109,15 @@ const App = () => {
   )
 
   const mapContacts = filteredContacts.map((contact) => (
-    <div key={contact.id}>
-      {contact.name} {contact.number}
-      <button id={contact.id} name={contact.name} onClick={handleRemove}>
+    <div key={contact.id} className="contact">
+      <div className="contact-name">{contact.name}</div>
+      <div className="contact-number">{contact.number}</div>
+      <button
+        id={contact.id}
+        name={contact.name}
+        onClick={handleRemove}
+        className="btn-delete"
+      >
         Remove
       </button>
     </div>
@@ -86,17 +125,18 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Notification notification={notification} error={error} />
+      <h1>Phonebook</h1>
       <Filter newQuery={newQuery} handleChange={handleChange} />
 
-      <h3>Add a Contact</h3>
+      <h1>Add a Contact</h1>
       <ContactForm
         newName={newName}
         handleChange={handleChange}
         handleClick={handleSubmit}
       />
 
-      <h2>Numbers</h2>
+      <h1>Numbers</h1>
       <Directory mapContacts={mapContacts} />
     </div>
   )
--- Part2/phonebook/src/Directory.js
+++ Part2/phonebook/src/Directory.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Directory = ({ mapContacts }) => {
-  return <div>{mapContacts}</div>
+  return <div className="directory">{mapContacts}</div>
 }
 
 export default Directory
